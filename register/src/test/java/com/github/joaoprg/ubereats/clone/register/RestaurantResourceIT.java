2c5c9bdb76b04a684e8646e429ba76f4cfcac491
joao-prg <joaopdrgoncalves@gmail.com>
2023-06-02 19:49:35 +0100

    + Added exception mappers.

--- register/src/test/java/com/github/joaoprg/ubereats/clone/register/RestaurantResourceIT.java
+++ register/src/test/java/com/github/joaoprg/ubereats/clone/register/RestaurantResourceIT.java
@@ -7,9 +7,12 @@ import com.github.database.rider.core.api.dataset.DataSet;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import org.approvaltests.Approvals;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import javax.ws.rs.core.Response;
+import java.util.stream.Stream;
 
 import static io.restassured.RestAssured.given;
 
@@ -19,11 +22,25 @@ import static io.restassured.RestAssured.given;
 @QuarkusTestResource(PostgresTestResource.class)
 public class RestaurantResourceIT {
 
-    @Test
+
+    private static Stream<Arguments> testGetRestaurantsOk() {
+        return Stream.of(
+                Arguments.of(1, 10),
+                Arguments.of(1, 1),
+                Arguments.of(2, 1)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testGetRestaurantsOk")
     @DataSet("get-restaurants.yml")
-    public void testBuscarRestaurantes() {
+    public void testGetRestaurantsOk(int page, int perPage) {
         String result = given()
-                .when().get("/restaurants")
+                .with()
+                .queryParam("page", page)
+                .queryParam("per_page", perPage)
+                .when()
+                .get("/restaurants")
                 .then()
                 .statusCode(Response.Status.OK.getStatusCode())
                 .extract().asString();
