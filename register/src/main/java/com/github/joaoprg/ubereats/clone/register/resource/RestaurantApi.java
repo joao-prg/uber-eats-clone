b9cbaa2ade7ccc90ecb30e7cced1d3b0fd06fb46
joao-prg <joaopdrgoncalves@gmail.com>
2023-06-11 17:18:40 +0100

    - Reverted Keycloack.

--- register/src/main/java/com/github/joaoprg/ubereats/clone/register/resource/RestaurantApi.java
+++ register/src/main/java/com/github/joaoprg/ubereats/clone/register/resource/RestaurantApi.java
@@ -23,15 +23,7 @@ import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -40,6 +32,7 @@ import java.util.UUID;
 
 @Path("/restaurants")
 @Tag(ref = "Restaurants API")
+@SecurityScheme(securitySchemeName = "ubereats-clone-oauth", type = SecuritySchemeType.OAUTH2, flows = @OAuthFlows(password = @OAuthFlow(tokenUrl = "http://localhost:8180/realms/ubereats-clone/protocol/openid-connect/token")))
 public interface RestaurantApi {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
